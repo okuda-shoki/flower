1ff5d254d390a0c94aec561b973d5e4c30276c6d
奥田将輝 <shoki0919kurosho@outlook.jp>
2021-04-24 13:02:19 +0900

    uncomplete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,12 @@
 <div class="container">
   <h1>花占いゲーム</h1>
   <p>花の枚数:{{item}}</p>
-  <p>現在の状況:{{word}}</p>
+  <p v-if="show">現在の状況:{{word}}</p>
+  <p v-else>結果:{{word}}</p>
   <button @click="dec()">花びらを減らす</button>
+
   </div>
+
 </template>
 
 <script>
@@ -14,31 +17,78 @@ export default {
     function getRandomIntInclusive() {
       return Math.floor(Math.random()*9)+2;
 }
-      let a=["嫌い","好き"];
-      let x;
+      var a=["嫌い","好き"];
+      var x;
       x=Math.floor(Math.random()*2); 
-      let document=a[x]
+      var document=a[x]
 return{
   item:getRandomIntInclusive(),
-  word:document
+  word:document,
+  show:true
 }
   },
   methods:{
     dec(){
-      if(this.item>0){
+      if(this.item>1){
         this.item--;
-        return this.item;
-      }
-      function add(){
-        this.item;
-        this.word=!this.word
-      }
-      return{
-        word:add()
-      }
-    }
-  }
+        if(this.word==="好き"){
+          this.word="嫌い"
+        }else{
+          this.word="好き";
+        }
+      }else if(this.item>0){
+        this.item--;
+        this.show=false;
+        if(this.word==="好き"){
+          this.word="嫌い"
+        }else{
+          this.word="好き";
+        }
+          return this.word
+      }else if(this.item==0){
+      this.$router.go({
+              path: this.$router.currentRoute.path,
+              force: true,
+            });
+            }
+      },
+      saveword(){
+        const parsed=JSON.stringify(this.word);
+        localStorage.setItem('word',parsed)
+      },
+      saveitem(){
+        const stored=JSON.stringify(this.item);
+        localStorage.setItem('item',stored)
+        },
+    },
+
+
+    mounted(){
+     if(localStorage.getItem('word')){
+       try{
+         this.word=JSON.parse(localStorage.getItem('word'))
+       }catch(e){
+         localStorage.removeItem('word')
+       }
+     }
+     if(localStorage.getItem('item')){
+       try{
+         this.item=JSON.parse(localStorage.getItem('item'))
+       }catch(e){
+         localStorage.removeItem('item')
+       }
+     }
+     var a = new Date();
+var b = new Date();
+console.log(b - a); //this works
+localStorage.a = a;
+localStorage.b = b;
+a = Date.parse(localStorage.a); // parse to date object
+b = Date.parse(localStorage.b);
+console.log(b - a);
+    },
 }
+  
 </script>
 
 <style scoped>
